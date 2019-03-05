c80d184edbf27789ec351a4f10defe0e2066d88e
kdwivedi <kritika.dwivedi@infostretch.com>
2019-03-05 16:51:34 +0530

    Updated script

--- src/main/java/CI_CD_Demo/Test01.java
+++ src/main/java/CI_CD_Demo/Test01.java
@@ -38,19 +38,4 @@ public void verifySearchButtonPass() {
 
     Assert.assertEquals(text, search_text, "Text not found!:(");
 }
-
-@Test
-public void verifySearchButtonFail() {
-
-    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-    driver.get("http://www.google.com");
-
-    String search_text = "Google Fearch";
-    WebElement search_button = driver.findElement(By.name("btnK"));
-
-    String text = search_button.getAttribute("value");
-
-    Assert.assertEquals(text, search_text, "Text not found!");
-}
 }
--- src/main/java/CI_CD_Demo/Test02.java
+++ src/main/java/CI_CD_Demo/Test02.java
@@ -8,11 +8,23 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 
 public class Test02{
 	private WebDriver driver;
+	
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\kritika.dwivedi\\Downloads\\Drivers\\chromedriver_win32\\chromedriver.exe");
+	    driver = new ChromeDriver();
+	}
+
+	@AfterClass
+	public void afterClass() {
+	    driver.quit();
+	}
 	
 	@Test
 	public void verifySearchButtonFail () {
